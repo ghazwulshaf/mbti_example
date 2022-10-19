d45ba745861eb4492806ac287dcbb1a0b4bfd957
Ghazwul Shaf <ghazwul.s@gmail.com>
2022-10-19 22:59:53 +0800

    add script.js for dashboard

---
+++ public/script.js
@@ -0,0 +1,13 @@
+const point_a = document.getElementById('point_a');
+const point_b = document.getElementById('point_b');
+const point_c = document.getElementById('point_c');
+
+function select_a_0() {
+    point_b.innerHTML = '1';
+    point_c.innerHTML = '2';
+}
+
+function select_a_0() {
+    point_b.innerHTML = '5';
+    point_c.innerHTML = '4';
+}
